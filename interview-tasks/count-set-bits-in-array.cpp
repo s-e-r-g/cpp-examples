8c3749bd21b9ce72b4e3ff141051a384fe48ed65
Serg Ponomarenko <sergnmi@gmail.com>
2017-11-13 18:00:55 +0200

    Update count-set-bits-in-array.cpp

--- interview-tasks/count-set-bits-in-array.cpp
+++ interview-tasks/count-set-bits-in-array.cpp
@@ -4,6 +4,32 @@
 #include <chrono>
 #include <vector>
 
+// Constants and tables
+
+constexpr std::uint8_t table8bit[256] =
+{
+    0,1,1,2,1,2,2,3,1,2,2,3,2,3,3,4,
+    1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,
+    1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,
+    2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
+    1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,
+    2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
+    2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
+    3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,
+    1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,
+    2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
+    2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
+    3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,
+    2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
+    3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,
+    3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,
+    4,5,5,6,5,6,6,7,5,6,6,7,6,7,7,8
+};
+
+std::vector<std::uint8_t> table16bit(65536);
+
+
+// Algorithms
 
 size_t calcDirectShift(const std::vector<std::uint64_t>& array)
 {
@@ -34,26 +60,6 @@ size_t calcDirectShiftOptimized(const std::vector<std::uint64_t>& array)
     return count;
 }
 
-constexpr std::uint8_t table8bit[256] =
-{
-    0,1,1,2,1,2,2,3,1,2,2,3,2,3,3,4,
-    1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,
-    1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,
-    2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
-    1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,
-    2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
-    2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
-    3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,
-    1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,
-    2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
-    2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
-    3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,
-    2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
-    3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,
-    3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,
-    4,5,5,6,5,6,6,7,5,6,6,7,6,7,7,8
-};
-
 size_t calc8bitTable(const std::vector<std::uint64_t>& array)
 {
     size_t count = 0;
@@ -78,10 +84,6 @@ size_t calc8bitTable(const std::vector<std::uint64_t>& array)
 
     return count;
 }
-
-
-std::vector<std::uint8_t> table16bit(65536);
-
 
 size_t calc8bitTableOptimized(const std::vector<std::uint64_t>& array)
 {
@@ -159,17 +161,16 @@ size_t calc64BitShift(const std::vector<std::uint64_t>& array)
     return totalCount;
 }
 
+constexpr std::uint64_t m1 = 0x5555555555555555;
+constexpr std::uint64_t m2 = 0x3333333333333333;
+constexpr std::uint64_t m3 = 0x0F0F0F0F0F0F0F0F;
+constexpr std::uint64_t m4 = 0x00FF00FF00FF00FF;
+constexpr std::uint64_t m5 = 0x0000FFFF0000FFFF;
+constexpr std::uint64_t m6 = 0x00000000FFFFFFFF;
+
 size_t calcTema(const std::vector<std::uint64_t>& data)
 {
-    constexpr std::uint64_t m1 = 0x5555555555555555;
-    constexpr std::uint64_t m2 = 0x3333333333333333;
-    constexpr std::uint64_t m3 = 0x0F0F0F0F0F0F0F0F;
-    constexpr std::uint64_t m4 = 0x00FF00FF00FF00FF;
-    constexpr std::uint64_t m5 = 0x0000FFFF0000FFFF;
-    constexpr std::uint64_t m6 = 0x00000000FFFFFFFF;
-
     std::uint64_t r32 = 0;
-
     for (auto value : data)
     {
         value = (value & m1) + ((value >> 1) & m1);
@@ -186,13 +187,6 @@ size_t calcTema(const std::vector<std::uint64_t>& data)
 
 size_t calcTema2(const std::vector<std::uint64_t>& data)
 {
-    constexpr std::uint64_t m1 = 0x5555555555555555;
-    constexpr std::uint64_t m2 = 0x3333333333333333;
-    constexpr std::uint64_t m3 = 0x0F0F0F0F0F0F0F0F;
-    constexpr std::uint64_t m4 = 0x00FF00FF00FF00FF;
-    constexpr std::uint64_t m5 = 0x0000FFFF0000FFFF;
-    constexpr std::uint64_t m6 = 0x00000000FFFFFFFF;
-
     std::uint64_t r32 = 0;
 
     const auto count = data.size();
@@ -239,6 +233,53 @@ size_t calcTema2(const std::vector<std::uint64_t>& data)
     return (r32 & m6) + ((r32 >> 32) & m6);
 }
 
+size_t calcTema3(const std::vector<std::uint64_t>& data)
+{
+    std::uint64_t r32 = 0;
+
+    const auto count = data.size();
+    const auto truncatedCount = count / 30 * 30;
+
+    std::uint64_t i = 0;
+    while (i < truncatedCount)
+    {
+        std::uint64_t r8 = 0;
+        for (std::uint8_t j = 0; j < 10; ++j)
+        {
+            auto v1 = data[i++];
+            auto v2 = data[i++];
+            const auto v3 = data[i++];
+
+            v1 = (v1 & m1) + ((v1 >> 1) & m1) + (v2 & m1);
+            v2 = (v3 & m1) + ((v3 >> 1) & m1) + ((v2 >> 1) & m1);
+
+            const auto r4 = (v1 & m2) + ((v1 >> 2) & m2) + (v2 & m2) + ((v2 >> 2) & m2);
+
+            r8 += (r4 & m3) + ((r4 >> 4) & m3);
+        }
+
+        r8 = (r8 & m4) + ((r8 >> 8) & m4);
+        r8 = (r8 & m5) + ((r8 >> 16) & m5);
+
+        r32 += r8;
+    }
+
+    for (; i < count; ++i)
+    {
+        auto value = data[i];
+        value = (value & m1) + ((value >> 1) & m1);
+        value = (value & m2) + ((value >> 2) & m2);
+        value = (value & m3) + ((value >> 4) & m3);
+        value = (value & m4) + ((value >> 8) & m4);
+        value = (value & m5) + ((value >> 16) & m5);
+
+        r32 += value;
+    }
+
+    return (r32 & m6) + ((r32 >> 32) & m6);
+}
+
+
 int main()
 {
     // Calculate tables
@@ -256,7 +297,7 @@ int main()
     std::vector<std::uint64_t> a(1000000);
     for (size_t i = 0; i < a.size(); ++i) a[i] = 0b1010101010101010101010101010101010101010101010101010101010101010;
 
-    // Preform tests
+    // Perform tests
     using Calculator = size_t (*)(const std::vector<std::uint64_t>&);
 
     struct Experiment
@@ -270,13 +311,18 @@ int main()
     std::vector<Experiment> experiments;
     experiments.push_back({"Direct Shift", calcDirectShift});
     experiments.push_back({"Direct Shift Optimized", calcDirectShiftOptimized});
+
+    experiments.push_back({"64bit Shift", calc64BitShift});
+
     experiments.push_back({"8bit Table", calc8bitTable});
     experiments.push_back({"8bit Table (opt)", calc8bitTableOptimized});
     experiments.push_back({"8bit Table (opt2)", calc8bitTableOptimizedV2});
-    experiments.push_back({"64bit Shift", calc64BitShift});
+
+    experiments.push_back({"16bit Table", calc16bitTable});
+
     experiments.push_back({"Tema shift", calcTema});
     experiments.push_back({"Tema2 shift", calcTema2});
-    experiments.push_back({"16bit Table", calc16bitTable});
+    experiments.push_back({"Tema3 shift", calcTema3});
 
     for (auto& experiment : experiments)
     {
@@ -285,6 +331,8 @@ int main()
         auto end = std::chrono::steady_clock::now();
         experiment.time = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
     }
+
+    // Output results
 
     std::sort(experiments.begin(), experiments.end(), [](const auto& a, const auto& b){return a.time < b.time;});
 
