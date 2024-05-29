613f3ffc91d6190c3e88c985debdcebf320b9f42
Serhii Ponomarenko <sergnmi@gmail.com>
2024-05-29 13:47:32 +0200

    Update switch-as-goto.cpp

--- strange-and-valid/switch-as-goto.cpp
+++ strange-and-valid/switch-as-goto.cpp
@@ -1,15 +1,25 @@
+#include <iostream>
+
+
 int main()
 {
-    int x = 100;
+    int x = 20;
     switch (x)
     {
         case 10:
         {
             std::cout << "point 1\n";
             case 20:
-                {
-                    std::cout << "point 2\n";
-                }
+            {
+                std::cout << "point 2\n";
+                goto label_abc;
+            }
+            
+            std::cout << "point 3\n";
+            
+            label_abc:
+            
+                std::cout << "point 4\n";
         }
     };
 }
